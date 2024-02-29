6e4fb8bf383643afa49f8310e28fbe5bb4c30140
Addisu544 <addisuagerie544@gmail.com>
2024-02-29 03:44:43 -0800

    withHTML

--- public/script.js
+++ public/script.js
@@ -1,26 +1,55 @@
-const button = document.querySelector("button");
-button.addEventListener("click", () => {
-  fetch("http://localhost:3000/create-checkout-session", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      items: [
-        { id: 1, quantity: 3 },
-        { id: 2, quantity: 1 },
-      ],
-    }),
-  })
-    .then((res) => {
-      if (res.ok) return res.json();
-      return res.json().then((json) => Promise.reject(json));
+// const button = document.querySelector("button");
+// button.addEventListener("click", () => {
+//   fetch("http://localhost:3000/create-checkout-session", {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify({
+//       items: [
+//         { id: 1, quantity: 3 },
+//         { id: 2, quantity: 1 },
+//       ],
+//     }),
+//   })
+//     .then((res) => {
+//       if (res.ok) return res.json();
+//       return res.json().then((json) => Promise.reject(json));
+//     })
+//     .then(({ url }) => {
+//       window.location = url;
+//       console.log(url);
+//     })
+//     .catch((e) => {
+//       console.error(e.error);
+//     });
+// });
+
+const buttons = document.querySelectorAll("button");
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const amount = button.dataset.amount;
+
+    fetch("http://localhost:3000/create-checkout-session", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        items: [{ id: 1, quantity: 1 }],
+        amount: parseInt(amount),
+      }),
     })
-    .then(({ url }) => {
-      window.location = url;
-      console.log(url);
-    })
-    .catch((e) => {
-      console.error(e.error);
-    });
+      .then((res) => {
+        if (res.ok) return res.json();
+        return res.json().then((json) => Promise.reject(json));
+      })
+      .then(({ url }) => {
+        window.location = url;
+        console.log(url);
+      })
+      .catch((e) => {
+        console.error(e.error);
+      });
+  });
 });
